c8544ddb02d6e9dbd60e48540289da60142ed7c2
sfdduncan <164909361+sfdduncan@users.noreply.github.com>
2025-06-12 19:39:09 -0400

    Updated Step 4 Programming
    Data and APIs # replace Create index.html
    Interactive Web submission # replace Description

--- weather_widget/javascript/main.js
+++ weather_widget/javascript/main.js
@@ -1,3 +1,22 @@
-console.log("this works");
+let input = document.querySelector(".zipcode");
+let btn = document.querySelector(".search-button");
 
- const API_KEY = config.WEATHER_API_KEY;
+getWeatherData = (zip) => {
+  let API_KEY = config.WEATHER_API_KEY;
+  let API_ENDPOINT = `https://api.openweathermap.org/data/2.5/weather?zip=${zip}&APPID=${API_KEY}`;
+
+  fetch(API_ENDPOINT)
+    .then((response) => response.json())
+    .then((data) => {
+      let local_weather_data = data;
+        console.log(local_weather_data)
+    })
+  }
+
+const getZipCode = (e) => {
+  e.preventDefault();
+  let ZIP_CODE = input.value;
+  getWeatherData(ZIP_CODE);
+};
+
+btn.addEventListener("click", getZipCode);
